3bf3abc842db674e068ed1e14e1064a96b004507
feixingjiadavid <139209569+feixingjiadavid@users.noreply.github.com>
2026-03-19 15:15:57 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // 💡 这是你全新的 Davis-Design-System-Pro 项目通行证
 const supabaseUrl = 'https://dhzyeqrbqrokwvfwform.supabase.co';
-const supabaseAnonKey = 'sb_publishable_BZYQh-m3mA0Hu7tBrdZKHA_XY-cDzjC'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoenllcXJicXJva3d2Zndmb3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NzYwODQsImV4cCI6MjA4OTQ1MjA4NH0.NFkgV_CI-VZwm2GDOwd_X7dYTwAotbh_e79tcXDKKwA'
 
 // 创建连接通道并暴露给其他页面使用
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
